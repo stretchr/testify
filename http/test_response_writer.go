870c90a638b9bb37d615ea17dec9c1f383d87e0b
Mat Ryer <matryer@mac.com>
2012-10-16 22:04:37 +0100

    added comments for the test response writer

--- http/test_response_writer.go
+++ http/test_response_writer.go
@@ -4,12 +4,21 @@ import (
 	"net/http"
 )
 
+// TestResponseWriter is a http.ResponseWriter object that keeps track of all activity
+// allowing you to make assertions about how it was used.
 type TestResponseWriter struct {
+
+	// WrittenHeaderInt is the last int written by the call to WriteHeader(int)
 	WrittenHeaderInt int
-	Output           string
-	header           http.Header
+
+	// Output is a string containing the written bytes using the Write([]byte) func.
+	Output string
+
+	// header is the internal storage of the http.Header object
+	header http.Header
 }
 
+// Header gets the http.Header describing the headers that were set in this response.
 func (rw *TestResponseWriter) Header() http.Header {
 
 	if rw.header == nil {
@@ -18,6 +27,8 @@ func (rw *TestResponseWriter) Header() http.Header {
 
 	return rw.header
 }
+
+// Write writes the specified bytes to Output.
 func (rw *TestResponseWriter) Write(bytes []byte) (int, error) {
 
 	// add these bytes to the output string
@@ -27,6 +38,8 @@ func (rw *TestResponseWriter) Write(bytes []byte) (int, error) {
 	return 0, nil
 
 }
+
+// WriteHeader stores the HTTP status code in the WrittenHeaderInt.
 func (rw *TestResponseWriter) WriteHeader(i int) {
 	rw.WrittenHeaderInt = i
 }
