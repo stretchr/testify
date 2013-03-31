cfc83347cf76864220aea3ae78e17d47c18b4a90
Tyler Bunnell <tylerbunnell@gmail.com>
2013-03-31 09:41:59 -0600

    Remove unreachable code

--- mock/mock.go
+++ mock/mock.go
@@ -409,8 +409,6 @@ func (args Arguments) String(indexOrNil ...int) string {
 		panic(fmt.Sprint("assert: arguments: Wrong number of arguments passed to String.  Must be 0 or 1, not %d", len(indexOrNil)))
 	}
 
-	return ""
-
 }
 
 // Int gets the argument at the specified index. Panics if there is no argument, or
