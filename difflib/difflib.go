a77cab6ef25e3a78c14c1b799cf2da92815d7664
Patrick Mezard <patrick@mezard.eu>
2013-11-23 21:16:25 +0100

    difflib: extract summary line from package documentation

--- difflib/difflib.go
+++ difflib/difflib.go
@@ -1,5 +1,6 @@
-// Package difflib is a partial port of Python difflib module. It provides tools
-// to compare sequences of strings and generate textual diffs.
+// Package difflib is a partial port of Python difflib module.
+//
+// It provides tools to compare sequences of strings and generate textual diffs.
 //
 // The following class and functions have been ported:
 //
