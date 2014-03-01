52f556e4214bb02c7908dce87ddbb92f8177da44
taichi <ryushi@gmail.com>
2014-03-01 11:10:44 +0900

    Simplify the API by using the new assertion.

--- suite/suite.go
+++ suite/suite.go
@@ -4,11 +4,14 @@ import (
 	"reflect"
 	"regexp"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // Suite is a basic testing suite with methods for storing and
 // retrieving the current *testing.T context.
 type Suite struct {
+	*assert.Assertions
 	t *testing.T
 }
 
@@ -20,6 +23,7 @@ func (suite *Suite) T() *testing.T {
 // SetT sets the current *testing.T context.
 func (suite *Suite) SetT(t *testing.T) {
 	suite.t = t
+	suite.Assertions = assert.New(t)
 }
 
 // Run takes a testing suite and runs all of the tests attached
