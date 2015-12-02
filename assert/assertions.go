d04dca765faa882597444f95b7084ba5ec44821b
Ernesto Jiménez <me@ernesto-jimenez.com>
2015-12-02 20:32:31 +0100

    Vendor dependencies and rewrite import paths

--- assert/assertions.go
+++ assert/assertions.go
@@ -14,8 +14,8 @@ import (
 	"unicode"
 	"unicode/utf8"
 
-	"github.com/davecgh/go-spew/spew"
-	"github.com/pmezard/go-difflib/difflib"
+	"github.com/stretchr/testify/Godeps/_workspace/src/github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/Godeps/_workspace/src/github.com/pmezard/go-difflib/difflib"
 )
 
 // TestingT is an interface wrapper around *testing.T
