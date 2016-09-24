1475a3d9ce3239211186e2d2ed49f3856fc5efec
Ernesto Jiménez <me@ernesto-jimenez.com>
2016-09-24 17:17:34 +0100

    Fixes #348 - Update go-spew

--- vendor/github.com/davecgh/go-spew/spew/bypass.go
+++ vendor/github.com/davecgh/go-spew/spew/bypass.go
@@ -13,9 +13,10 @@
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 // NOTE: Due to the following build constraints, this file will only be compiled
-// when the code is not running on Google App Engine and "-tags disableunsafe"
-// is not added to the go build command line.
-// +build !appengine,!disableunsafe
+// when the code is not running on Google App Engine, compiled by GopherJS, and
+// "-tags safe" is not added to the go build command line.  The "disableunsafe"
+// tag is deprecated and thus should not be used.
+// +build !js,!appengine,!safe,!disableunsafe
 
 package spew
 
