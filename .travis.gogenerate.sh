cc189738c010cb847790782fd5f87d4e0002da9d
Ernesto Jiménez <me@ernesto-jimenez.com>
2017-05-29 13:47:57 +0100

    Fix gogenerate travis check

--- .travis.gogenerate.sh
+++ .travis.gogenerate.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-go generate
+if [[ "$TRAVIS_GO_VERSION" =~ ^1\.[45](\..*)?$ ]]; then
+  exit 0
+fi
+
+go get github.com/ernesto-jimenez/gogen/imports
+go generate ./...
 if [ -n "$(git diff)" ]; then
   echo "Go generate had not been run"
   git diff
