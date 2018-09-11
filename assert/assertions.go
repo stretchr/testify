f2347ac6c9c98ca14694f64f92c4b917954a6dc9
Harald Nordgren <haraldnordgren@gmail.com>
2018-09-11 20:52:35 +0200

    Allow assert.Equal on string type alias without panicking on failure

--- assert/assertions.go
+++ assert/assertions.go
@@ -1345,7 +1345,7 @@ func diff(expected interface{}, actual interface{}) string {
 	}
 
 	var e, a string
-	if ek != reflect.String {
+	if et != reflect.TypeOf("") {
 		e = spewConfig.Sdump(expected)
 		a = spewConfig.Sdump(actual)
 	} else {
