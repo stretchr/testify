56db5c973017e1011eebdaa39263b19b4fa13a2c
apantykhin <apantykhin@gmail.com>
2018-12-25 00:33:35 +0400

    add isordered

--- assert/assertions.go
+++ assert/assertions.go
@@ -493,6 +493,203 @@ func Empty(t TestingT, object interface{}, msgAndArgs ...interface{}) bool {
 
 }
 
+func compare(obj1, obj2 interface{}, kind reflect.Kind) int {
+	switch kind {
+	case reflect.Int:
+		{
+			intobj1 := obj1.(int)
+			intobj2 := obj2.(int)
+			if intobj1 > intobj2 {
+				return -1
+			}
+			if intobj1 == intobj2 {
+				return 0
+			}
+			if intobj1 < intobj2 {
+				return 1
+			}
+		}
+	case reflect.Int8:
+		{
+			int8obj1 := obj1.(int8)
+			int8obj2 := obj2.(int8)
+			if int8obj1 > int8obj2 {
+				return -1
+			}
+			if int8obj1 == int8obj2 {
+				return 0
+			}
+			if int8obj1 < int8obj2 {
+				return 1
+			}
+		}
+	case reflect.Int16:
+		{
+			int16obj1 := obj1.(int16)
+			int16obj2 := obj2.(int16)
+			if int16obj1 > int16obj2 {
+				return -1
+			}
+			if int16obj1 == int16obj2 {
+				return 0
+			}
+			if int16obj1 < int16obj2 {
+				return 1
+			}
+		}
+	case reflect.Int32:
+		{
+			int32obj1 := obj1.(int32)
+			int32obj2 := obj2.(int32)
+			if int32obj1 > int32obj2 {
+				return -1
+			}
+			if int32obj1 == int32obj2 {
+				return 0
+			}
+			if int32obj1 < int32obj2 {
+				return 1
+			}
+		}
+	case reflect.Int64:
+		{
+			int64obj1 := obj1.(int64)
+			int64obj2 := obj2.(int64)
+			if int64obj1 > int64obj2 {
+				return -1
+			}
+			if int64obj1 == int64obj2 {
+				return 0
+			}
+			if int64obj1 < int64obj2 {
+				return 1
+			}
+		}
+	case reflect.Float32:
+		{
+			float32obj1 := obj1.(float32)
+			float32obj2 := obj2.(float32)
+			if float32obj1 > float32obj2 {
+				return -1
+			}
+			if float32obj1 == float32obj2 {
+				return 0
+			}
+			if float32obj1 < float32obj2 {
+				return 1
+			}
+		}
+	case reflect.Float64:
+		{
+			float64obj1 := obj1.(float64)
+			float64obj2 := obj2.(float64)
+			if float64obj1 > float64obj2 {
+				return -1
+			}
+			if float64obj1 == float64obj2 {
+				return 0
+			}
+			if float64obj1 < float64obj2 {
+				return 1
+			}
+		}
+	case reflect.String:
+		{
+			stringobj1 := obj1.(string)
+			stringobj2 := obj2.(string)
+			if stringobj1 > stringobj2 {
+				return -1
+			}
+			if stringobj1 == stringobj2 {
+				return 0
+			}
+			if stringobj1 < stringobj2 {
+				return 1
+			}
+		}
+	}
+
+	return 0
+}
+
+// isOrdered checks that collection contains only orderable elements and in the one order.
+func isOrdered(object interface{}, direction int) (ok bool, isOrdered bool) {
+	objKind := reflect.TypeOf(object).Kind()
+	if objKind != reflect.Slice && objKind != reflect.Array {
+		return false, false
+	}
+
+	objValue := reflect.ValueOf(object)
+	objLen := objValue.Len()
+
+	if objLen == 0 {
+		return false, false
+	}
+
+	if objLen == 1 {
+		return true, true
+	}
+
+	firstValueKind := objValue.Index(0).Kind()
+	for i := 1; i < objLen; i++ {
+		value := objValue.Index(i)
+		valueKind := value.Kind()
+
+		if firstValueKind != valueKind {
+			return false, false
+		}
+
+		prevValue := objValue.Index(i - 1)
+		compareResult := compare(prevValue.Interface(), value.Interface(), firstValueKind)
+
+		if direction != 0 && compareResult != 0 && direction != compareResult {
+			return true, false
+		}
+
+		if direction == 0 && compareResult != 0 {
+			direction = compareResult
+		}
+	}
+
+	return true, true
+}
+
+// IsOrdered asserts that the specified object is ordered
+func IsOrdered(t TestingT, object interface{}, msgAndArgs ...interface{}) bool {
+	if h, ok := t.(tHelper); ok {
+		h.Helper()
+	}
+
+	ok, ordered := isOrdered(object, 0)
+	if !ok {
+		return Fail(t, fmt.Sprintf("\"%s\" could not be applied builtin ordering", object), msgAndArgs...)
+	}
+
+	if ordered {
+		Fail(t, fmt.Sprintf("Should be ordered, but was %v", object), msgAndArgs...)
+	}
+
+	return ordered
+}
+
+// IsNotOrdered asserts that the specified object is not ordered
+func IsNotOrdered(t TestingT, object interface{}, msgAndArgs ...interface{}) bool {
+	if h, ok := t.(tHelper); ok {
+		h.Helper()
+	}
+
+	ok, ordered := isOrdered(object, 0)
+	if !ok {
+		return Fail(t, fmt.Sprintf("\"%s\" could not be applied builtin ordering", object), msgAndArgs...)
+	}
+
+	if !ordered {
+		Fail(t, fmt.Sprintf("Should be not ordered, but was %v", object), msgAndArgs...)
+	}
+
+	return ordered
+}
+
 // NotEmpty asserts that the specified object is NOT empty.  I.e. not nil, "", false, 0 or either
 // a slice or a channel with len == 0.
 //
