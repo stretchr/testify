bf25442124d4a7b0596e22d6a82dcf54c2f68fb5
Bo Sunesen <bos@uber.com>
2019-08-28 16:13:23 +0200

    Use Repeatability as tie-breaker for closest match

--- mock/mock.go
+++ mock/mock.go
@@ -281,6 +281,7 @@ func (m *Mock) findExpectedCall(method string, arguments ...interface{}) (int, *
 
 func (m *Mock) findClosestCall(method string, arguments ...interface{}) (*Call, string) {
 	var diffCount int
+	var repeatability int
 	var closestCall *Call
 	var err string
 
@@ -288,8 +289,9 @@ func (m *Mock) findClosestCall(method string, arguments ...interface{}) (*Call, 
 		if call.Method == method {
 
 			errInfo, tempDiffCount := call.Arguments.Diff(arguments)
-			if tempDiffCount < diffCount || diffCount == 0 {
+			if tempDiffCount < diffCount || diffCount == 0 || (tempDiffCount == diffCount && call.Repeatability > repeatability) {
 				diffCount = tempDiffCount
+				repeatability = call.Repeatability
 				closestCall = call
 				err = errInfo
 			}
