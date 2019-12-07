b4bc8de52a8a6117bbc751c1d47804dfd0f87b9d
Leigh McCulloch <leigh@mcchouse.com>
2019-12-07 13:12:57 -0800

    Make the go generate script better and more similar to PR 853

--- .travis.gogenerate.sh
+++ .travis.gogenerate.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_GO_VERSION" =~ ^1\.(12|13)(\..*)?$ ]]; then
+# If GOMOD is defined we are running with Go Modules enabled, either
+# automatically or via the GO111MODULE=on environment variable. Codegen only
+# works with modules, so skip generation if modules is not in use.
+if [[ ! -z "$(go env GOMOD)" ]]; then
   exit 0
 fi
 
-GO111MODULE=on go generate ./...
+go generate ./...
 if [ -n "$(git diff)" ]; then
   echo "Go generate had not been run"
   git diff
