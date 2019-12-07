ab4cb3b5274ca6fb0cc3ce822784e07428a18189
Leigh McCulloch <leigh@mcchouse.com>
2019-12-07 00:39:52 -0800

    Limit go generate to 1.12 and 1.13

--- .travis.gogenerate.sh
+++ .travis.gogenerate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [[ "$TRAVIS_GO_VERSION" =~ ^1\.(12|13)(\..*)?$ ]]; then
+  exit 0
+fi
+
 GO111MODULE=on go generate ./...
 if [ -n "$(git diff)" ]; then
   echo "Go generate had not been run"
