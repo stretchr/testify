7c5ac23b811357c13e5cf9e4db7d3e0a67702731
Leigh McCulloch <leigh@mcchouse.com>
2019-12-07 00:07:40 -0800

    Make _codegen its own module so that its dependencies do not pollute the root modules dependency graph

--- assert/assertions.go
+++ assert/assertions.go
@@ -22,7 +22,7 @@ import (
 	yaml "gopkg.in/yaml.v2"
 )
 
-//go:generate go run ../_codegen/main.go -output-package=assert -template=assertion_format.go.tmpl
+//go:generate sh -c "cd ../_codegen && go build && cd - && ../_codegen/_codegen -output-package=assert -template=assertion_format.go.tmpl"
 
 // TestingT is an interface wrapper around *testing.T
 type TestingT interface {
