2684bea53bebb3cf7165d29e286870e087eaee49
Boyan Soubachov <boyanvs@gmail.com>
2020-01-28 22:25:54 +1100

    Check go generate on builds * Check if we have mismatching `go generate ./...` output on builds

--- .travis.gofmt.sh
+++ .travis.gofmt.sh
@@ -5,3 +5,10 @@ if [ -n "$(gofmt -l .)" ]; then
   gofmt -d .
   exit 1
 fi
+
+go generate ./...
+if [ -n "$(git status -s -uno)" ]; then
+  echo "Go generate output does not match commit."
+  echo "Did you forget to run go generate ./... ?"
+  exit 1
+fi
