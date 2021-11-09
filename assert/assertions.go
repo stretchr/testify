4a1999634fc3f05affb3166e0c1aeae82e9d2853
Adam Luzsi <adamluzsi@gmail.com>
2021-11-09 22:01:40 +0100

    refactor .Empty assertion for array types

--- assert/assertions.go
+++ assert/assertions.go
@@ -565,10 +565,6 @@ func isEmpty(object interface{}) bool {
 	// collection types are empty when they have no element
 	case reflect.Chan, reflect.Map, reflect.Slice:
 		return objValue.Len() == 0
-	// array types are empty when they match their zero-initialized state
-	case reflect.Array:
-		zero := reflect.Zero(objValue.Type()).Interface()
-		return ObjectsAreEqual(zero, object)
 	// pointers are empty if nil or if the value they point to is empty
 	case reflect.Ptr:
 		if objValue.IsNil() {
@@ -576,7 +572,8 @@ func isEmpty(object interface{}) bool {
 		}
 		deref := objValue.Elem().Interface()
 		return isEmpty(deref)
-		// for all other types, compare against the zero value
+	// for all other types, compare against the zero value
+	// array types are empty when they match their zero-initialized state
 	default:
 		zero := reflect.Zero(objValue.Type())
 		return reflect.DeepEqual(object, zero.Interface())
