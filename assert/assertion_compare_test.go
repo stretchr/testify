c29de713426fdf9068696c483705fdbb24a815ac
Ilia Kravets <ilia@springsw.com>
2022-01-20 11:25:52 +0200

    add tests for correct msgAndArgs forwarding
    len(msgAndArgs)>1 should lead to fmt.Sprintf()

--- assert/assertion_compare_test.go
+++ assert/assertion_compare_test.go
@@ -428,3 +428,21 @@ func Test_containsValue(t *testing.T) {
 		Equal(t, currCase.result, compareResult)
 	}
 }
+
+func TestComparingMsgAndArgsForwarding(t *testing.T) {
+	msgAndArgs := []interface{}{"format %s %x", "this", 0xc001}
+	expectedOutput := "format this c001\n"
+	funcs := []func(t TestingT){
+		func(t TestingT) { Greater(t, 1, 2, msgAndArgs...) },
+		func(t TestingT) { GreaterOrEqual(t, 1, 2, msgAndArgs...) },
+		func(t TestingT) { Less(t, 2, 1, msgAndArgs...) },
+		func(t TestingT) { LessOrEqual(t, 2, 1, msgAndArgs...) },
+		func(t TestingT) { Positive(t, 0, msgAndArgs...) },
+		func(t TestingT) { Negative(t, 0, msgAndArgs...) },
+	}
+	for _, f := range funcs {
+		out := &outputT{buf: bytes.NewBuffer(nil)}
+		f(out)
+		Contains(t, out.buf.String(), expectedOutput)
+	}
+}
