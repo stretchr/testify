192cfd963c235513d0a36aa753f4c7de0631b95f
Edward Raigosa <edraigos@microsoft.com>
2022-06-23 23:47:47 +0000

    fix go vet copy lock by using pointer

--- mock/mock.go
+++ mock/mock.go
@@ -519,7 +519,7 @@ func AssertExpectationsForObjects(t TestingT, testObjects ...interface{}) bool {
 		h.Helper()
 	}
 	for _, obj := range testObjects {
-		if m, ok := obj.(Mock); ok {
+		if m, ok := obj.(*Mock); ok {
 			t.Logf("Deprecated mock.AssertExpectationsForObjects(myMock.Mock) use mock.AssertExpectationsForObjects(myMock)")
 			obj = &m
 		}
