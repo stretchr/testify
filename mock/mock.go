1cfa497aa0b2bf67eb140778df3b073987edf6ab
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-07-14 13:09:38 +0100

    Fix race condition in printing of diff values.

--- mock/mock.go
+++ mock/mock.go
@@ -886,7 +886,7 @@ func (args Arguments) Diff(objects []interface{}) (string, int) {
 			actualFmt = "(Missing)"
 		} else {
 			actual = objects[i]
-			actualFmt = fmt.Sprintf("(%[1]T=%[1]v)", actual)
+			actualFmt = fmt.Sprintf("(%[1]T=%[1]p)", actual)
 		}
 
 		if len(args) <= i {
@@ -894,7 +894,7 @@ func (args Arguments) Diff(objects []interface{}) (string, int) {
 			expectedFmt = "(Missing)"
 		} else {
 			expected = args[i]
-			expectedFmt = fmt.Sprintf("(%[1]T=%[1]v)", expected)
+			expectedFmt = fmt.Sprintf("(%[1]T=%[1]p)", expected)
 		}
 
 		if matcher, ok := expected.(argumentMatcher); ok {
