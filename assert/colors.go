2d756387a42c1495ab6652ebceff37b5082b3cde
EwenQuim <ewen.quimerch@gmail.com>
2023-09-06 18:12:21 +0200

    Better coloring performance This allows the compiler to inline the ANSI codes and to save 2 bytes/op, reduucing the perf at 7 bytes/op instead of 9 bytes/op. Knowing that the original code only uses 3 bytes/op, there is still space for improvement

--- assert/colors.go
+++ assert/colors.go
@@ -7,22 +7,24 @@ import (
 	"golang.org/x/term"
 )
 
-const redMark = "\033[0;31m"
-const greenMark = "\033[0;32m"
-const endMark = "\033[0m"
+const (
+	redMark   = "\033[0;31m"
+	greenMark = "\033[0;32m"
+	endMark   = "\033[0m"
+)
+
+var isTerminal = term.IsTerminal(int(os.Stdout.Fd()))
 
 func redColored(i interface{}) interface{} {
 	if isTerminal {
-		return redMark + fmt.Sprintf("%s", i) + endMark
+		return fmt.Sprintf(redMark+"%s"+endMark, i)
 	}
 	return i
 }
 
 func greenColored(i interface{}) interface{} {
 	if isTerminal {
-		return greenMark + fmt.Sprintf("%s", i) + endMark
+		return fmt.Sprintf(greenMark+"%s"+endMark, i)
 	}
 	return i
 }
-
-var isTerminal = term.IsTerminal(int(os.Stdout.Fd()))
