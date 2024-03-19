c0026653f80b1a64596cc3a36d65d68849830621
Olivier Mengué <dolmen@cpan.org>
2024-03-19 21:35:26 +0100

    assert: make YAML dependency pluggable via build tags
    Make the YAML dependency required for {assert,require}.YAMLEq{,f} pluggable.
    
    The implementation can be selected using build tags:
    - testify_yaml_default (default): gopkg.in/yaml.v3 is used, like before
    - testify_yaml_fail: YAML deserialization is not implemented and always
      fails. So assert.YAMLEq always fails. This is useful if the test suite
      package doesn't use assert.YAMLEq (very common case).
    - testify_yaml_custom: the github.com/stretchr/testify/assert/yaml
      package exposes an Unmarshal variable of type func([]byte, any) error
      (same as gopkg.in/yaml.v3) that allows to plug any alternate
      implementation. For example github.com/goccy/go-yaml.Unmarshal.
      This allows to avoid the link constraints of the license of
      gopkg.in/yaml.v3 (see PR stretchr/testify#1120).
    
    Usage: go test -tags testify_yaml_fail
    
    To install the alternate implementation with testify_yaml_custom:
    
    	//go:build testify_yaml_custom
    
    	package my_pkg_test
    
    	import (
    		goyaml "github.com/goccy/go-yaml"
    		"github.com/stretchr/testify/assert/yaml"
    	)
    
    	func init() {
    		yaml.Unmarshal = goyaml.Unmarshal
    	}

--- assert/assertions.go
+++ assert/assertions.go
@@ -19,7 +19,9 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/pmezard/go-difflib/difflib"
-	"gopkg.in/yaml.v3"
+
+	// Wrapper around gopkg.in/yaml.v3
+	"github.com/stretchr/testify/assert/yaml"
 )
 
 //go:generate sh -c "cd ../_codegen && go build && cd - && ../_codegen/_codegen -output-package=assert -template=assertion_format.go.tmpl"
