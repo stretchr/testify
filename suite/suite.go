bc7459ec38128532ff32f23cfab4ea0b725210f2
Olivier Mengué <dolmen@cpan.org>
2025-05-27 15:36:15 +0200

    suite: faster filtering of methods (-testify.m)
    Refactor filtering of methods in suite.Run: the regexp given via
    -testify.m flag is compiled just once, out of the loop.

--- suite/suite.go
+++ suite/suite.go
@@ -138,16 +138,24 @@ func Run(t *testing.T, suite TestingSuite) {
 	methodFinder := reflect.TypeOf(suite)
 	suiteName := methodFinder.Elem().Name()
 
-	for i := 0; i < methodFinder.NumMethod(); i++ {
-		method := methodFinder.Method(i)
-
-		ok, err := methodFilter(method.Name)
+	var matchMethodRE *regexp.Regexp
+	if *matchMethod != "" {
+		var err error
+		matchMethodRE, err = regexp.Compile(*matchMethod)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "testify: invalid regexp for -m: %s\n", err)
 			os.Exit(1)
 		}
+	}
 
-		if !ok {
+	for i := 0; i < methodFinder.NumMethod(); i++ {
+		method := methodFinder.Method(i)
+
+		if !strings.HasPrefix(method.Name, "Test") {
+			continue
+		}
+		// Apply -testify.m filter
+		if matchMethodRE != nil && !matchMethodRE.MatchString(method.Name) {
 			continue
 		}
 
@@ -215,15 +223,6 @@ func Run(t *testing.T, suite TestingSuite) {
 	}()
 
 	runTests(t, tests)
-}
-
-// Filtering method according to set regular expression
-// specified command-line argument -m
-func methodFilter(name string) (bool, error) {
-	if !strings.HasPrefix(name, "Test") {
-		return false, nil
-	}
-	return regexp.MatchString(*matchMethod, name)
 }
 
 func runTests(t *testing.T, tests []test) {
