stretchr/testify

commit b5ce16571001d6e96e1693ac891fed5c2510c651

fixing panic in calls to assertion with nil m.mutex (#1212)
* fixing panic in calls to assertion with nil m.mutex

This reverts a change that was made in https://github.com/stretchr/testify/pull/1182
The PR makes m.mutex a pointer which now needs to be checked but it's not checked for nil everywhere.

This should also help with these issues:
- https://github.com/stretchr/testify/issues/1208
- https://github.com/stretchr/testify/issues/1210

* Revert throwing out the lock because other concurrent calls can already have it locked

* fix go vet copy lock by using pointer

* fix obj assignment for passing test

mock/mock.go
@@ -255,7 +255,7 @@ type Mock struct {
 	// this data completely allowing you to do whatever you like with it.
 	testData objx.Map
 
-	mutex *sync.Mutex
+	mutex sync.Mutex
 }
 
 // String provides a %v format string for Mock.
@@ -282,10 +282,6 @@ func (m *Mock) TestData() objx.Map {
 
 // Test sets the test struct variable of the mock object
 func (m *Mock) Test(t TestingT) {
-	if m.mutex == nil {
-		m.mutex = &sync.Mutex{}
-	}
-
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
 	m.test = t
@@ -315,9 +311,6 @@ func (m *Mock) On(methodName string, arguments ...interface{}) *Call {
 			panic(fmt.Sprintf("cannot use Func in expectations. Use mock.AnythingOfType(\"%T\")", arg))
 		}
 	}
-
-	// Since we start mocks with the .On() function, m.mutex should be reset
-	m.mutex = &sync.Mutex{}
 
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
@@ -526,9 +519,9 @@ func AssertExpectationsForObjects(t TestingT, testObjects ...interface{}) bool {
 		h.Helper()
 	}
 	for _, obj := range testObjects {
-		if m, ok := obj.(Mock); ok {
+		if m, ok := obj.(*Mock); ok {
 			t.Logf("Deprecated mock.AssertExpectationsForObjects(myMock.Mock) use mock.AssertExpectationsForObjects(myMock)")
-			obj = &m
+			obj = m
 		}
 		m := obj.(assertExpectationser)
 		if !m.AssertExpectations(t) {
@@ -544,9 +537,6 @@ func AssertExpectationsForObjects(t TestingT, testObjects ...interface{}) bool {
 func (m *Mock) AssertExpectations(t TestingT) bool {
 	if h, ok := t.(tHelper); ok {
 		h.Helper()
-	}
-	if m.mutex == nil {
-		m.mutex = &sync.Mutex{}
 	}
 
 	m.mutex.Lock()

commit 0bf6b946d985309f37a4364b0b1f01a92698730e

mock.AssertExpectationsForObjects fix panic with wrong testObject type.
Previously passing a mock.Mock by value rather than by reference worked (in cases without mutex locking issues) and logged a warning. This was broken by #1212 which introduced a breaking change in an attempt to fix go vet.

There is no clean way to fix the breaking change as we now have (and want) go vet in our CI.

This PR does not revert the breaking change but changes the panic to a test failure with a useful message.

mock/mock.go
@@ -603,11 +603,12 @@ func AssertExpectationsForObjects(t TestingT, testObjects ...interface{}) bool {
 		h.Helper()
 	}
 	for _, obj := range testObjects {
-		if m, ok := obj.(*Mock); ok {
-			t.Logf("Deprecated mock.AssertExpectationsForObjects(myMock.Mock) use mock.AssertExpectationsForObjects(myMock)")
-			obj = m
+		m, ok := obj.(assertExpectationiser)
+		if !ok {
+			t.Errorf("Invalid test object type %T. Expected reference to a mock.Mock, eg: 'AssertExpectationsForObjects(t, myMock)' or 'AssertExpectationsForObjects(t, &myMock.Mock)'", obj)
+			continue
+
 		}
-		m := obj.(assertExpectationiser)
 		if !m.AssertExpectations(t) {
 			t.Logf("Expectations didn't match for Mock: %+v", reflect.TypeOf(m))
 			return false

mock/mock_test.go
@@ -2461,3 +2461,9 @@ func TestIssue1785ArgumentWithMutatingStringer(t *testing.T) {
 	m.MethodCalled("Method", &mutatingStringer{N: 2})
 	m.AssertExpectations(t)
 }
+
+func TestIssue1227AssertExpectationsForObjectsWithMock(t *testing.T) {
+	mockT := &MockTestingT{}
+	AssertExpectationsForObjects(mockT, Mock{})
+	assert.Equal(t, 1, mockT.errorfCount)
+}