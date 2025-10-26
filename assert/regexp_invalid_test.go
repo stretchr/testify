e58e326988b5257c9c358d61e3d18b200d529622
kdt523 <krushna.datir231@vit.edu>
2025-10-26 15:58:00 +0530

    test(assert): gofmt tabs in regexp_invalid_test.go to satisfy CI formatting check

--- assert/regexp_invalid_test.go
+++ assert/regexp_invalid_test.go
@@ -5,15 +5,15 @@ import "testing"
 // Verifies that invalid patterns no longer cause a panic when using Regexp/NotRegexp.
 // Instead, the assertion should fail and return false.
 func TestRegexp_InvalidPattern_NoPanic(t *testing.T) {
-    NotPanics(t, func() {
-        mockT := new(testing.T)
-        False(t, Regexp(mockT, "\\C", "whatever"))
-    })
+	NotPanics(t, func() {
+		mockT := new(testing.T)
+		False(t, Regexp(mockT, "\\C", "whatever"))
+	})
 }
 
 func TestNotRegexp_InvalidPattern_NoPanic(t *testing.T) {
-    NotPanics(t, func() {
-        mockT := new(testing.T)
-        False(t, NotRegexp(mockT, "\\C", "whatever"))
-    })
+	NotPanics(t, func() {
+		mockT := new(testing.T)
+		False(t, NotRegexp(mockT, "\\C", "whatever"))
+	})
 }
