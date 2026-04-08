89f7ac61e71c2a5531952a441eb33b8b0d53112d
Olivier Mengué <dolmen@cpan.org>
2026-04-08 19:00:37 +0200

    mock: drop objx.Map: "Mock.TestData() objx.Map" now returns TestData
    Drop the last reference to dependency github.com/stretchr/objx.
    
    Reference to objx.Map is removed (BREAKING CHANGE) from the
    Mock.TestData method signature. A similar object is returned instead of
    objx.Map, but we do not aim to implement the full objx.Value API:
    instead we expose the reflect.Value API that should help enough the few
    users of Mock.TestData to move away from objx.Map reliance.
    
    Implemented as planned in #1852.

--- mock/mock_objx.go
+++ mock/mock_objx.go
@@ -6,16 +6,63 @@
 
 package mock
 
-import "github.com/stretchr/objx"
+import (
+	"reflect"
+)
 
-type testData = objx.Map
+type testData = TestData
 
 // TestData holds any data that might be useful for testing.  Testify ignores
 // this data completely allowing you to do whatever you like with it.
-func (m *Mock) TestData() objx.Map {
+//
+// Deprecated: do not use. The new TestData do not return an [github.com/stretchr/objx.Map] anymore.
+// See https://github.com/stretchr/testify/issues/1852.
+func (m *Mock) TestData() TestData {
 	if m.testData == nil {
-		m.testData = make(objx.Map)
+		m.testData = make(TestData)
 	}
 
 	return m.testData
+}
+
+// TestData replaces [github.com/stretchr/objx.Map].
+type TestData map[string]interface{}
+
+type reflectValue = reflect.Value
+
+// TestDataValue replaces [github.com/stretchr/objx.Value] and exposes the same methods as [reflect.Value].
+// Only a subset of objx.Value methods are available.
+type TestDataValue struct {
+	reflectValue
+}
+
+// Set replaces [github.com/stretchr/objx.Map.Set].
+func (td TestData) Set(selector string, v interface{}) {
+	td[selector] = v
+}
+
+// Get replaces [github.com/stretchr/objx.Map.Get].
+func (td TestData) Get(selector string) *TestDataValue {
+	v, ok := td[selector]
+	if !ok {
+		return nil
+	}
+	return &TestDataValue{reflectValue: reflect.ValueOf(&v).Elem()}
+}
+
+// MustInter replaces [github.com/stretchr/objx.Value.MustInter].
+func (v *TestDataValue) MustInter() interface{} {
+	if v == nil {
+		return nil
+	}
+	// v.reflectValue contains an interface (ex: error), so dereference it
+	return v.reflectValue.Elem()
+}
+
+// MustInter replaces [github.com/stretchr/objx.Value.Data].
+func (v *TestDataValue) Data() interface{} {
+	if v == nil {
+		return nil
+	}
+	return v.reflectValue.Interface()
 }
